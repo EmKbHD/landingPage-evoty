85ccc1a793ada9588727be89e7b7137eb8c4cb41
EmmaKbl <emmakbl@gmail.com>
2024-11-26 13:44:14 +0200

    configuration of the carousel

--- Components/Testimonials.tsx
+++ Components/Testimonials.tsx
@@ -1,79 +1,103 @@
+"use client";
+
 import { TestimonialsData } from "../db/data";
-import Slider from "react-slick";
+// import Slider from "react-slick";
 import { Box, Container, Flex, Image, Text } from "@chakra-ui/react";
 
 const Testimonials = () => {
-  const settingCarousel = {
-    dots: true,
-    arrow: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    // autoplay set to true
-    autoplaySpeed: 2000,
-    cssEase: "linear",
-    pauseOnHover: true,
-    pauseOnFocus: true,
-    responsive: [
-      {
-        breakpoint: 10000,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-          infinite: true,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          initialSlide: 2,
-        },
-      },
-      {
-        breakpoint: 640,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+  // const settingCarousel = {
+  //   dots: true,
+  //   arrows: false,
+  //   infinite: true,
+  //   speed: 500,
+  //   slidesToShow: 1,
+  //   slidesToScroll: 1,
+  //   // autoplay: true,
+  //   autoplaySpeed: 2000,
+  //   cssEase: "linear",
+  //   pauseOnHover: true,
+  //   pauseOnFocus: true,
+  //   responsive: [
+  //     {
+  //       breakpoint: 10000,
+  //       settings: {
+  //         slidesToShow: 3,
+  //         slidesToScroll: 1,
+  //         infinite: true,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 1024,
+  //       settings: {
+  //         slidesToShow: 2,
+  //         slidesToScroll: 1,
+  //         initialSlide: 2,
+  //       },
+  //     },
+  //     {
+  //       breakpoint: 640,
+  //       settings: {
+  //         slidesToShow: 1,
+  //         slidesToScroll: 1,
+  //       },
+  //     },
+  //   ],
+  // };
 
   return (
-    <Box>
-      <Container className="container">
+    <Box border="1px solid #E5E5E5" py={14} mb={10}>
+      <Container mx="auto" px={["1rem", "2rem", "4rem", "5rem", "6rem"]} py={8}>
         {/* header section start here */}
-        <Text as="h2">
+        <Text
+          as="h2"
+          textAlign="left"
+          mb={10}
+          fontSize={{ base: "2xl", md: "3xl", lg: "4xl" }}
+          fontWeight="bold"
+        >
           What are the Customers
           <br /> Saying about us ?
         </Text>
-        {/* testimonials customers card start here */}
-        <Slider {...settingCarousel}>
-          {TestimonialsData.map((item) => (
-            <Box key={item.id}>
-              <Flex>
-                <Flex>
-                  <Image
-                    src={item.img}
-                    alt="profile-img"
-                    className="w-20 h-20 rounded-full"
-                  />
-                  <Flex className="flex flex-col justify-center ml-4">
-                    <strong>{item.name}</strong>
-                    <p className="text-sm">{item.role}</p>
-                  </Flex>
+        {/* Testimonial customers Carousel  cards start here */}
+        {/* <Slider {...settingCarousel}> */}
+        {TestimonialsData.map((item) => (
+          <Box key={item.id} my={6}>
+            <Flex
+              flexDir="column"
+              mx={2}
+              rounded="lg"
+              shadow="0 10px 15px -3px rgb(0 0 0 / 0.2)"
+              bg="primary/5"
+              p={4}
+            >
+              <Flex mb={2}>
+                <Image
+                  src={item.img}
+                  alt="profile-img"
+                  w={20}
+                  h={20}
+                  rounded="full"
+                />
+                <Flex flexDir="column" ml={4} justifyContent="center">
+                  <Text as="span" fontWeight="bold">
+                    {item.name}
+                  </Text>
+                  <Text as="p" fontSize="sm">
+                    {item.role}
+                  </Text>
                 </Flex>
-                <Box>
-                  <Text as="p">{item.desc}</Text>
-                  <Text as="p">{item.rating}</Text>
-                </Box>
               </Flex>
-            </Box>
-          ))}
-        </Slider>
+              <Box py={2} spaceY={4}>
+                <Text as="p" fontSize="sm" color="gray.500">
+                  {item.desc}
+                </Text>
+                {/* Ratings (e.g., stars) */}
+                <Text as="p">{item.rating}</Text>
+              </Box>
+            </Flex>
+          </Box>
+        ))}
+        {/* </Slider> */}
       </Container>
     </Box>
   );
