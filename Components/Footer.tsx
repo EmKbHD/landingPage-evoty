07b8c6f2d57872f06d21b0fec8a12f679ff512dd
EmmaKbl <emmakbl@gmail.com>
2024-11-27 23:18:44 +0200

    adjusting grid and subgrid

--- Components/Footer.tsx
+++ Components/Footer.tsx
@@ -1,6 +1,14 @@
 "use client";
 
-import { Box, Container, Image, Link, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Container,
+  Grid,
+  GridItem,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
 import { NavbarMenu } from "../db/data";
 import Footer_CompanyInfo from "./Footer_CompanyInfo";
 
@@ -24,30 +32,69 @@ const TermsPolicies = [
 
 const Footer = () => {
   return (
-    <Box border="1px solid #E5E5E5">
-      <Container>
-        {/* company info here */}
-        <Box>
-          <Box>
+    <Box
+      bgGradient="to-r"
+      gradientFrom="brandPrimary.800"
+      gradientTo="brandPrimary.900"
+      borderTopRadius="2xl"
+    >
+      <Container mx="auto" px={["1rem", "2rem", "4rem", "5rem", "6rem"]} py={8}>
+        <Grid
+          border="1px solid #E5E5E5"
+          templateColumns={{
+            base: "repeat(2, 1fr 2fr)",
+            md: "repeat(2, 1fr 3fr)",
+          }}
+          gap={{ base: 2, md: 4 }}
+          py={5}
+          borderTop={2}
+          borderColor="gray.300/10"
+          color="white"
+        >
+          {/* Company info here */}
+          <GridItem
+            border="1px solid #E5E5E5"
+            minW={{ base: "155px", md: "200px" }}
+            py={8}
+            px={4}
+            spaceY={4}
+          >
             <Box>
               <Image
                 src="/assets/eVoty.svg"
                 alt="eVoty Logo"
                 maxHeight="4rem"
+                bg="white"
+                rounded="lg"
+                px={2}
               />
             </Box>
 
-            <Text as="p">
+            <Text
+              as="p"
+              color="white"
+              textAlign={{ base: "justify", md: "left" }}
+            >
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. In
-              numquam distinctio aperiam amet culpa?
+              numquam distinctio ?
             </Text>
+          </GridItem>
 
-            {/* links start here */}
-            <Box>
+          {/* Links start here */}
+          <GridItem colSpan={3}>
+            <Grid
+              border="1px solid #E5E5E5"
+              gridTemplateColumns={{
+                base: "repeat(2, 1fr)",
+                lg: "repeat(3, 1fr)",
+              }}
+            >
               {/* Important links start here */}
-              <Box>
-                <Text as="h2">Important Links</Text>
-                <Box as="ul">
+              <GridItem border="1px solid #E5E5E5" py={8} px={4}>
+                <Text as="h2" fontSize="lg" fontWeight="bold" mb={4}>
+                  Important Links
+                </Text>
+                <Box as="ul" display="flex" flexDir="column" gap={3}>
                   {NavbarMenu.map((item) => {
                     return (
                       <li key={item.id}>
@@ -56,11 +103,13 @@ const Footer = () => {
                     );
                   })}
                 </Box>
-              </Box>
+              </GridItem>
               {/* Terms and Policies  */}
-              <Box>
-                <Text as="h2">Resources</Text>
-                <Box as="ul">
+              <GridItem border="1px solid #E5E5E5" py={8} px={4}>
+                <Text as="h2" fontSize="lg" fontWeight="bold" mb={4}>
+                  Resources
+                </Text>
+                <Box as="ul" display="flex" flexDir="column" gap={3}>
                   {TermsPolicies.map((item) => {
                     return (
                       <li key={item.id}>
@@ -69,18 +118,20 @@ const Footer = () => {
                     );
                   })}
                 </Box>
-              </Box>
+              </GridItem>
               {/* Socials link start here */}
-              <Footer_CompanyInfo />
-            </Box>
-          </Box>
-          {/* copyright area  */}
-          <Box>
-            <Box>
-              <Text as="span">
-                &copy; Copyright 2024 &nbsp; Afritic Group. All rights reserved.
-              </Text>
-            </Box>
+              <GridItem border="1px solid #E5E5E5" py={8} px={4}>
+                <Footer_CompanyInfo />
+              </GridItem>
+            </Grid>
+          </GridItem>
+        </Grid>
+        {/* copyright area  */}
+        <Box border="1px solid #E5E5E5" w="full" py={8} px={4}>
+          <Box border="1px solid #E5E5E5" textAlign="center">
+            <Text as="span" color="white">
+              &copy; Copyright 2024 &nbsp; Afritic Group. All rights reserved.
+            </Text>
           </Box>
         </Box>
       </Container>
--- Components/Footer_CompanyInfo.tsx
+++ Components/Footer_CompanyInfo.tsx
@@ -2,7 +2,7 @@ import { Box, Link, Text } from "@chakra-ui/react";
 import {
   FaFacebook,
   FaInstagram,
-  FaTwitter,
+  FaXTwitter,
   FaLinkedin,
 } from "react-icons/fa6";
 
@@ -12,35 +12,51 @@ const Socials = [
     icon: FaFacebook,
     title: "Facebook",
     link: "#",
+    hoverColor: "blue",
   },
   {
     id: 2,
     icon: FaInstagram,
     title: "Instagram",
     link: "#",
+    hoverColor: "red",
   },
   {
     id: 3,
     title: "Twitter",
-    icon: FaTwitter,
+    icon: FaXTwitter,
     link: "#",
+    hoverColor: "black",
   },
   {
     id: 4,
     icon: FaLinkedin,
     title: "Linkedin",
     link: "#",
+    hoverColor: "blue",
   },
 ];
 
 const Footer_CompanyInfo = () => {
   return (
     <Box>
-      <Text as="h2">Social Medias Links</Text>
-      <Box as="ul">
+      <Text as="h2" fontSize="lg" fontWeight="bold" mb={4}>
+        Social Medias Links
+      </Text>
+      <Box as="ul" display="flex" flexDir="column" gap={3}>
         {Socials.map((social) => (
-          <Link href={social.link} key={social.id}>
-            {<social.icon size={20} />}
+          <Link
+            href={social.link}
+            key={social.id}
+            gap={2}
+            _hover={{ color: "brandSecondary.400" }}
+          >
+            <Text
+              as="span"
+              _hover={{ color: `${social.hoverColor} !important` }}
+            >
+              {<social.icon size={20} />}
+            </Text>
             {social.title}
           </Link>
         ))}
